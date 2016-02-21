158c4277e8fe1e542e1b79c5b004861721323c66
TJ Draper <tj@buzzingpixel.com>
2016-02-21 16:01:46 -0600

    Add base add-on files

---
+++ system/user/addons/last_edit_date/pi.last_edit_date.php
@@ -0,0 +1,19 @@
+<?php if (! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Last Edit Date plugin
+ *
+ * @package last_edit_date
+ * @author TJ Draper <tj@buzzingpixel.com>
+ * @link https://buzzingpixel.com/ee-add-ons/last-edit-date
+ * @copyright Copyright (c) 2016, Buzzing Pixel
+ */
+
+class Last_edit_date
+{
+	public function __construct()
+	{
+		var_dump('here');
+		die;
+	}
+}
